30b26f80ede9cafce281ef21f7e9d8ac8d653e10
Marisa Raeder <mraeder11@gmail.com>
2020-09-21 08:35:27 -0500

    Workshop 2 Take 2

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,66 +1,78 @@
-const express = require('express'); 
-const bodyParser = require('body-parser'); 
+const express = require('express');   // express middleware 
+const bodyParser = require('body-parser');  // body parser middleware
 const Partner = require('../models/partner');   // update the response to each defined endpoint using the new Partner and Promotion Model
 
 const partnerRouter = express.Router();
 
-partnerRouter.use(bodyParser.json());
+partnerRouter.use(bodyParser.json());    // using body parser middleware
 
 partnerRouter.route('/')             // Create a Node module named partnerRouter.js that will implement the Express router for /partners and /partners/:partnerId
-.get((req, res, next) => {
-    Partner.find()
-    .then(partners => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(partners);
+    .get((req, res, next) => {           // get request to get all docs in collection 
+        Partner.find()
+        .then(partners => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(partners);
+        })
+        .catch(err => next(err));
     })
-    .catch(err => next(err));
-.post((req, res, next) => {
-    Partner.create(req.body)
-    .then(partner => {
-        console.log('Partner created ', partner);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(partner)
+    .post((req, res, next) => {         // create new doc in partner collection 
+        Partner.create(req.body)
+        .then(partner => {
+            console.log('Partner created ', partner);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(partner)
+        })
+        .catch(err => next(err));
     })
-    .catch(err => next(err));
-.delete((req, res, next) => {
-    Partner.deleteMany()
-    .then(response => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(response);
+    .put((req, res) => {              // put request that is not supported
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /partners');
     })
-    .catch(err => next(err));
-});
+    .delete((req, res, next) => {
+        Partner.deleteMany()
+        .then(response => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(response);
+        })
+        .catch(err => next(err));
+    })
 
-partnerRouter.route('/:partnerId')
-.get((req, res, next) => {
-Partner.findById(req.params.partnerId)
-.then(partner => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(partner);
-})
-.catch(err => next(err));
-.put((req, res, next) => {
-    Partner.findByIdAndUpdate(req.params.partnerId, {
-        $set: req.body
-    }, { new: true })
-    .then(partner => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(partner);
+    partnerRouter.route('/:partnerId') 
+    .get((req, res, next) => {          // get request to get all partners with ID matching requested ID
+        Partner.findById(req.params.partnerId)
+        .then(partner => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(partner);
+        })
+        .catch(err => next(err));
     })
-    .catch(err => next(err));
-.delete((req, res, next) => {
-    Partner.findByIdAndDelete(req.params.partnerId)
-    .then(response => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(response);
+    .post((req, res) => { // post request that is not supported
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /partners/${req.params.partnerId}`); 
     })
-    .catch(err => next(err));
-});
+    .put((req, res, next) => {            // put request updating any partners with ID matching ID requested
+        Partner.findByIdAndUpdate(req.params.partnerId, {
+            $set: req.body
+        }, { new: true })
+        .then(partner => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(partner); 
+        })
+        .catch(err => next(err)); 
+    })
+    .delete((req, res, next) => {        // delete request for deleting any partners with ID matching ID requested
+        Partner.findByIdAndDelete(req.params.partnerId)
+        .then(response => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(response);
+        })
+        .catch(err => next(err)); 
+    })
 
 module.exports = partnerRouter;
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,66 +1,78 @@
-const express = require('express');                // In the node-express/routes folder, create a Node module named promotionRouter.js that will implement the Express router for /promotions and /promotions/:promotionId
-const bodyParser = require('body-parser');
-const Promotion = require('../models/promotion');
+const express = require('express');          // In the node-express/routes folder, create a Node module named promotionRouter.js that will implement the Express router for /promotions and /promotions/:promotionId
+const bodyParser = require('body-parser');   // using body parser middleware 
+const Promotion = require('../models/promotion');  // promotion model 
 
 const promotionRouter = express.Router();
 
-promotionRouter.use(bodyParser.json());
+promotionRouter.use(bodyParser.json());   // declaring use of body parser middleware
 
-promotionRouter.route('/')                         // Endpoints: Write a route() method on the router for each of the paths above, just as you did with the campsiteRouter...     
-.get((req, res, next) => {
-    Promotion.find()
-    .then(promotions => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(promotions);
+promotionRouter.route('/')               // Endpoints: Write a route() method on the router for each of the paths above, just as you did with the campsiteRouter...     
+    .get((req, res, next) => {     // get request to get all docs in the collection
+        Promotion.find()
+        .then(promotions => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotions);
+        })
+        .catch(err => next(err)); 
     })
-    .catch(err => then(err));
-.post((req, res, next) => {
-    Promotion.create(req.body)
-    .then(promotion => {
-        console.log('Promotion created ', promotion);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(promotion);
+    .post((req, res, next) => {     // create new doc in promotion collection
+        Promotion.create(req.body)
+        .then(promotion => {
+            console.log('Promotion Created ', promotion);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion);
+        })
+        .catch(err => next(err)); 
     })
-    .catch(err => then(err));
-.delete((req, res, next) => {
-    Promotion.deleteMany()
-    .then(response => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(response);
+    .put((req, res) => {          // put request that is not supported
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /promotions');
     })
-    .catch(err => then(err));
-});
+    .delete((req, res, next) => {     // delete request for deleting any docs in promotion collection
+        Promotion.deleteMany()
+        .then(response => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(response);
+        })
+        .catch(err => next(err)); 
+    })
 
-promotionRouter.route('/:promotionId')
-.get((req, res, next) => {
-    Promotion.findById(req.params.promotionId)
-    .then(promotion => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(promotion);
+    promotionRouter.route('/:promotionId') 
+    .get((req, res, next) => {            // get request for getting all promotions with ID matching requested ID
+        Promotion.findById(req.params.promotionId)
+        .then(promotion => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion);
+        })
+        .catch(err => next(err));
     })
-    .catch(err => then(err));
-.put((req, res, next) => {
-    Promotion.findByIdAndUpdate(req.params.promotionId, {
-        $set: req.body
-    }, { new: true })
-    .then(promotion => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(promotion);
+    .post((req, res) => { // post request that is not supported
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /promotions/${req.params.promotionId}`); 
     })
-    .catch(err => then(err));
-.delete((req, res) => {
-    Promotion.findByIdAndDelete(req.params.promotionId)
-    .then(response => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(response);
+    .put((req, res, next) => {         // put request for updating any promotions with ID matching ID requested
+        Promotion.findByIdAndUpdate(req.params.promotionId, {
+            $set: req.body
+        }, { new: true })
+        .then(promotion => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion); 
+        })
+        .catch(err => next(err)); 
     })
-    .catch(err => then(err));
-});
+    .delete((req, res, next) => {        // delete request for deleting any promotions with ID matching ID requested
+        Promotion.findByIdAndDelete(req.params.promotionId)
+        .then(response => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(response);
+        })
+        .catch(err => next(err)); 
+    })
 
 module.exports = promotionRouter;
